f408558842277924fb9d16389c0bd4fbb17c9905
Jamie Ohax <jimohax@gmail.com>
2023-02-02 06:50:29 +0100

    connected client with server

--- Server/index.js
+++ Server/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
-import PostRoute from './Routes/PostRoute.js';
+import PostRoute from "./Routes/PostRoute.js";
 
 // Routes
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json({ limit: "100000", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "100000", extended: true }));
+app.use(cors());
 
 dotenv.config();
 mongoose.set("strictQuery", true);
--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,112 +1,153 @@
 import React from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logIn, signUp } from "../../Actions/AuthAction";
 
 const Auth = () => {
-  return (
-    <div className="Auth">
-      <div className="A-left">
-        <img src={Logo} alt="" />
-        <div className="webName">
-          <h1>Jamie Media</h1>
-          <h6>Jump outside the box</h6>
-        </div>
-      </div>
+	const [isSignUp, setIsSignUp] = useState(true);
+	const dispatch = useDispatch();
+	const loading = useSelector((state) => state.AuthReducer.loading);
+	console.log(loading);
+	const [data, setData] = useState({
+		firstname: "",
+		lastname: "",
+		password: "",
+		confirmpass: "",
+		username: "",
+	});
 
-      <LogIn />
-      {/* <Signup /> */}
+	const [confirmPass, setConfirmPass] = useState(true);
 
-    </div>
-  );
+	const handleChange = (e) => {
+		setData({ ...data, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (isSignUp) {
+			data.password === data.confirmpass
+				? dispatch(signUp(data))
+				: setConfirmPass(false);
+		} else {
+			dispatch(logIn(data));
+		}
+	};
+
+	const resetForm = () => {
+		setConfirmPass(true);
+		setData({
+			firstname: "",
+			lastname: "",
+			password: "",
+			confirmpass: "",
+			username: "",
+		});
+	};
+	return (
+		// left side
+		<div className="Auth">
+			<div className="A-left">
+				<img src={Logo} alt="" />
+				<div className="webName">
+					<h1>Jamie Media</h1>
+					<h6>Jump outside the box</h6>
+				</div>
+			</div>
+
+			{/* right side */}
+			<div className="A-right">
+				<form
+					action=""
+					className="infoForm authForm"
+					onSubmit={handleSubmit}
+				>
+					<h3>{isSignUp ? "Sign up" : "Log in"}</h3>
+
+					{isSignUp && (
+						<div>
+							<input
+								type="text"
+								placeholder="First Name"
+								className="infoInput"
+								name="firstname"
+								onChange={handleChange}
+								value={data.firstname}
+							/>
+							<input
+								type="text"
+								placeholder="Last Name"
+								className="infoInput"
+								name="lastname"
+								onChange={handleChange}
+								value={data.lastname}
+							/>
+						</div>
+					)}
+
+					<div>
+						<input
+							type="text"
+							placeholder="User Name"
+							className="infoInput"
+							name="username"
+							onChange={handleChange}
+							value={data.username}
+						/>
+					</div>
+					<div>
+						<input
+							type="password"
+							placeholder="Password"
+							className="infoInput"
+							name="password"
+							onChange={handleChange}
+							value={data.password}
+						/>
+						{isSignUp && (
+							<input
+								type="password"
+								placeholder="Confirm Password"
+								className="infoInput"
+								name="confirmpass"
+								onChange={handleChange}
+								value={data.confirmpass}
+							/>
+						)}
+					</div>
+					<span
+						style={{
+							display: confirmPass ? "none" : "block",
+							color: "red",
+							fontSize: "12px",
+							alignSelf: "flex-end",
+							marginRight: "5px",
+						}}
+					>
+						* Passwords do not match
+					</span>
+					<div>
+						<span
+							style={{ fontSize: "12px", cursor: "pointer" }}
+							onClick={() => {
+								setIsSignUp((prev) => !prev);
+								resetForm();
+							}}
+						>
+							{isSignUp
+								? "Already have an account? Login!"
+								: "Don't have an account? SignUp!"}
+						</span>
+					</div>
+					<button className="button infoButton" type="submit" disabled={loading}>
+						{ loading ? "Loading..." : isSignUp ? "SignUp" : "Login"}
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 };
-
-function LogIn() {
-  return (
-    <div className="A-right">
-      <form action="" className="infoForm authForm wide">
-        <h3>Log In</h3>
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            className="infoInput"
-            name="firstname"
-          />
-          
-        </div>
-        
-        <div>
-          <input
-            type="text"
-            placeholder="Password"
-            className="infoInput"
-            name="password"
-          />
-          
-        </div>
-
-        <div>
-          <span style={{fontSize: "12px"}}>Don't have an account? Sign Up!</span>
-        </div>
-        <button className="button infoButton" type="submit">
-        LogIn
-        </button>
-      </form>
-    </div>
-  );
-}
-
-function Signup() {
-  return (
-    <div className="A-right">
-      <form action="" className="infoForm authForm">
-        <h3>Sign up</h3>
-        <div>
-          <input
-            type="text"
-            placeholder="First Name"
-            className="infoInput"
-            name="firstname"
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="infoInput"
-            name="lastname"
-          />
-        </div>
-        <div>
-        <input
-            type="text"
-            placeholder="User Name"
-            className="infoInput"
-            name="username"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Password"
-            className="infoInput"
-            name="password"
-          />
-          <input
-            type="text"
-            placeholder="Confirm Password"
-            className="infoInput"
-            name="confirmpassword"
-          />
-        </div>
-
-        <div>
-          <span style={{fontSize: "12px"}}>Already have an account. Login!</span>
-        </div>
-        <button className="button infoButton" type="submit">
-          SignUp
-        </button>
-      </form>
-    </div>
-  );
-}
 
 export default Auth;
