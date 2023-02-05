aeecf0013d419f5d45ab0f6b15e37df38b235e75
Jamie Ohax <jimohax@gmail.com>
2023-02-05 14:20:33 +0100

    more changes

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import FollowersCard from '../FollowersCard/FollowersCard'
-import LogoSearch from '../Logosearch/LogoSearch'
-import ProfileCard from '../ProfileCard/ProfileCard'
-import './Profile.css'
+import React from "react";
+import FollowersCard from "../FollowersCard/FollowersCard";
+import LogoSearch from "../Logosearch/LogoSearch";
+import ProfileCard from "../ProfileCard/ProfileCard";
+import "./Profile.css";
 
 const Profile = () => {
-  return (
-    <div className="Profile">
-        <LogoSearch/>
-        <ProfileCard/>
-        <FollowersCard/>
-    </div>
-  )
-}
+	return (
+		<div className="Profile">
+			<LogoSearch />
+			<ProfileCard />
+			<FollowersCard />
+		</div>
+	);
+};
 
-export default Profile
+export default Profile;
